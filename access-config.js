d8bcad50c4d8f82fc3cb421a15208854dbf1c488
donatusdonobiekezie <donatusdonobiekezie@gmail.com>
2026-09-12 17:57:10 +0100

    Add files via upload

---
+++ access-config.js
@@ -0,0 +1,7 @@
+// ACCESS configuration placeholder.
+// Do not put Paystack secret keys, passwords, OTPs, card details or other secrets here.
+// The public Supabase URL and anon/publishable key can be added here later after the project is created.
+window.ACCESS_CONFIG = {
+  SUPABASE_URL: "",
+  SUPABASE_ANON_KEY: ""
+};
