94536665edb4291b8eabe4e15789f69bda4580cd
donatusdonobiekezie <donatusdonobiekezie@gmail.com>
2026-09-12 21:52:35 +0100

    Update access-config.js

--- access-config.js
+++ access-config.js
@@ -1,5 +1,5 @@
 window.ACCESS_CONFIG = {
-  SUPABASE_URL: "https://awjimlkecsjidsrvquo.supabase.co",
+  SUPABASE_URL: "https://https://awjmllkecsjidsrzvquo.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_xyBh2puqqhh2CmDghWl_CQ_DR7_lm3j"
 };
 // Never put a Supabase secret/service_role key, database password,
